303dfd08194191db1c1ba8740c48a759e80d0003
Chad Wilson <mrchadmwilson@gmail.com>
2021-06-10 19:11:59 -0400

    Added everything else from old website

---
+++ js/get_recent.js
@@ -0,0 +1,83 @@
+
+    const divResult = document.getElementById("divResult")
+
+    async function getRepos() {
+        clear()
+       //alert("test")
+        const url = "https://api.github.com/search/repositories?q=user:CWilsonTSP"
+        const response = await fetch(url)
+        const result = await response.json()
+
+      console.log(result)
+      console.log(typeof result)
+
+      let repos = result.items // Your repo data
+        repos.sort((a, b) => { return a.create_at - b.create_at; });   
+        repos.reverse();
+      //result.items.forEach(i=>console.log(i.full_name))
+      for(p = 0; p < 3; p++){
+          // github link
+          // wiki link
+          // image link
+          // description link
+          // title 
+
+          const github_link = document.createElement("a")
+          github_link.className = "greenbox"
+          github_link.href = repos[p].html_url;
+          github_link.textContent = "GitHub"
+
+          const github_wiki = document.createElement("a")
+          if (repos[p].has_wiki === true){
+            
+              github_wiki.className = "purplebox"
+              github_wiki.href = repos[p].html_url + "/wiki";
+              github_wiki.textContent = "Wiki"
+          }
+
+          const github_release = document.createElement("a")
+          github_release.className = "bluebox"
+          github_release.href = repos[p].html_url + "/releases";
+          github_release.textContent = "Downloads"
+
+          const item_list = document.createElement("li")
+          item_list.appendChild(github_release)
+          item_list.appendChild(github_link)
+          if (repos[p].has_wiki === true){
+            item_list.appendChild(github_wiki)
+          }
+
+
+          const project_image = document.createElement("img")
+          project_image.src = "https://raw.githubusercontent.com/" + repos[p].full_name + "/master/index.png"
+          project_image.alt = repos[p].name + " image"
+
+          const project_name = document.createElement("p")
+          project_name.className = "header"
+          project_name.innerText = repos[p].name
+
+          const project_desc = document.createElement("p")
+          project_desc.className = "body"
+          project_desc.innerText = repos[p].description
+
+          const project_caption = document.createElement("figcaption")
+          project_caption.style = "text-align: center"
+          project_caption.appendChild(project_name)
+          project_caption.appendChild(project_desc)
+          project_caption.appendChild(document.createElement("br"))
+          project_caption.appendChild(item_list)
+
+          const project = document.createElement("figure")
+          project.className = "grid-item"
+          project.appendChild(project_image)
+          project.appendChild(project_caption)
+
+
+          divResult.appendChild(project)   
+      };
+      
+  }
+  function clear(){
+      while(divResult.firstChild)
+          divResult.removeChild(divResult.firstChild)
+  }
